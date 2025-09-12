75a9e614b38c92457b2f98a2fb33ffeb577d55cb
abhishek0286 <mukul0286@gmail.com>
2025-09-12 17:28:16 +0530

    Update

--- src/modules/orders/routes/orderRoutes.js
+++ src/modules/orders/routes/orderRoutes.js
@@ -4,7 +4,8 @@ const verifyJWT = require("../../../middlewares/verifyJWT");
 const upload = require("../../../middlewares/multer");
 const orderController = require("../controller/orderController");
 
-orderRouter.post("/place-order",verifyJWT.decodeToken,upload.none(),orderController.placeOrder);
+// orderRouter.post("/place-order",verifyJWT.decodeToken,upload.none(),orderController.placeOrder);
+orderRouter.post("/place-order",verifyJWT.decodeToken,upload.none(),orderController.placeMultipalOrder);
 orderRouter.get("/get-my-all-orders",verifyJWT.decodeToken,upload.none(),orderController.getMyOrders);
 orderRouter.get("/my-orders/:orderId",verifyJWT.decodeToken,upload.none(),orderController.getOrderDetail);
 orderRouter.post("/cancel-order/:orderId",verifyJWT.decodeToken,upload.none(),orderController.cancelOrder);
